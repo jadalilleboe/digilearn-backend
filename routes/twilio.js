d78495daa3e43a8c9895a2a3b9def4ecc898c231
Jada Lilleboe <82007190+jadalilleboe@users.noreply.github.com>
2022-01-22 23:46:25 -0600

    update

--- routes/twilio.js
+++ routes/twilio.js
@@ -23,4 +23,24 @@ twilioRouter.post('/', function(req, res, next) {
   })
 });
 
+twilioRouter.options('/', function(req, res, next) {
+  const receiver = req.body.number
+  res.header('Content-Type', 'application/json')
+  const randomFunFacts = ["ff1", "ff2", 'ff3', 'ff4', 'ff5']
+  const funFact = randomFunFacts[Math.floor(Math.random() * (4))]
+  client.messages.create({
+      from: config.TWILIO_PHONE_NUMBER,
+      to: receiver,
+      body: funFact
+  })
+  .then(() => {
+      response.send(JSON.stringify({ success: true}))
+  })
+  .catch(() => {
+      response.send(JSON.stringify({ success: false }))
+  })
+});
+
+
+
 module.exports = twilioRouter;
